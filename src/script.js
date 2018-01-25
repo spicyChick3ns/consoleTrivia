48e90f9d2c65789eb85de249419812b92ec3921f
Melissa Gonzales <gonzalesmsd@gmail.com>
2018-01-25 07:50:43 -0500

    initial commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function Q(question, answers, correctAnswer) {
+  this.question = question;
+  this.answers = answer;
+  this.correctAnswer = correctAnswer;
+}
+var q1 = new Q('Is JS the coolest programing language in the world?', ['Yes','No'], 0);
